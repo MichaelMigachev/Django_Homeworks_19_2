9606545ade0a08fa5862b14acfb3ae124dfc4506
MichaelMigachev <migachev21@gmail.com>
2024-12-25 17:38:19 +0300

    add

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,11 @@
+# шаблонный фильтр
+from django import template
+
+register = template.Library()
+
+
+@register.filter()
+def media_filter(path):
+    if path:
+        return f"/media/{path}"
+    return "#"
